38fbb1a85dade4dea19191fad79a997454383a23
liyongjie <yj.li@muheda.com>
2018-08-23 15:37:18 +0800

    section

--- ZhiHuDayNews/ZhiHuDayNews/Controller/HomeViewController.swift
+++ ZhiHuDayNews/ZhiHuDayNews/Controller/HomeViewController.swift
@@ -13,12 +13,15 @@ import RxSwift
 import RxCocoa
 import RxDataSources
 import ZCycleView
+import SwiftDate
 
 class HomeViewController: UIViewController {
     
     //MARK: property
     
     var newsDate = ""
+    var titleNum = Variable(0)
+    
     let dataArr = Variable([SectionModel<String,StoryModel>]())
     let dataSource = RxTableViewSectionedReloadDataSource<SectionModel<String,StoryModel>>(configureCell: {(dataSource,tv,indexPath,model) in
         let cell = tv.dequeueReusableCell(withIdentifier: "homeListCellId") as! HomeListTableViewCell
@@ -39,6 +42,7 @@ class HomeViewController: UIViewController {
         banner.timeInterval = 2
         banner.isAutomatic = true
         banner.isInfinite = true
+        banner.imageContentMode = .scaleAspectFill
         return banner
     }()
     var homeTableView:UITableView = {
@@ -81,7 +85,18 @@ class HomeViewController: UIViewController {
             .setDelegate(self)
             .disposed(by: disposeBag)
         
-        
+        titleNum
+            .asObservable()
+            .distinctUntilChanged()
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { (num) in
+                if num == 0 {
+                    self.navView.titleLab.text = "今日要闻"
+                } else {
+                    self.navView.titleLab.text = self.dataSource[num].model
+                }
+            })
+            .disposed(by: disposeBag)
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -107,6 +122,7 @@ class HomeViewController: UIViewController {
                 }
                 self.bannerView.setUrlsGroup(imgArr, titlesGroup: titleArr)
                 self.newsDate = model.date!
+                self.navView.refreshView.endRefresh()
             })
             .disposed(by: disposeBag)
     }
@@ -127,10 +143,36 @@ class HomeViewController: UIViewController {
 
 extension HomeViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        
+        if indexPath.section == dataArr.value.count - 1 && indexPath.row == 0 {
+            requestMoreData()
+        }
+        self.titleNum.value = (tableView.indexPathsForVisibleRows?.reduce(Int.max) { (result, ind) -> Int in return min(result, ind.section) })!
+
+    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if section > 0 {
+            return UILabel().then {
+                $0.frame = CGRect(x: 0, y: 0, width: ScreenWidth, height: 38)
+                $0.backgroundColor = UIColor.rgb(63, 141, 208)
+                $0.textColor = .white
+                $0.font = UIFont.systemFont(ofSize: 15)
+                $0.textAlignment = .center
+                $0.text = self.dataSource[section].model
+            }
+        }
+        return UIView()
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 90
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if section == 0 {
+            return 0
+        }
+        return 38
+    }
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 0.01
     }
     
 }
@@ -138,16 +180,26 @@ extension HomeViewController: UITableViewDelegate {
 extension HomeViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) { 
         navView.backgroundColor = UIColor.colorFromHex(0x3F8DD0).withAlphaComponent(scrollView.contentOffset.y / 200)
-
+        navView.refreshView.pullToRefresh(progress: -scrollView.contentOffset.y / 64)
+    }
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if scrollView.contentOffset.y <= -64 {
+            navView.refreshView.beginRefresh {
+                self.requestData()
+            }
+        }
+    }
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        navView.refreshView.resetLayer()
     }
 }
 
 extension HomeViewController: ZCycleViewProtocol {
     func cycleViewDidScrollToIndex(_ index: Int) {
-        print("滚动到了\(index+1)图片")
+        
     }
     func cycleViewDidSelectedIndex(_ index: Int) {
-        print("点击了\(index+1)图片")
+        print("点击了\(index)张图片")
     }
 }
 
